7552c470ac1ea0d31de8dcd5c050f7d997920b4c
equipa6 <equip_a6@boscdelacoma.cat>
2022-05-06 19:54:03 +0200

    commit

--- Clogin.py
+++ Clogin.py
@@ -6,8 +6,17 @@ chat_ventana = Tk()
 chat_ventana.title("Clogin")
 chat_ventana.geometry("1131x668")
 
+# FRAME DE LES CONFIGURACIONS GENERALS----------------------------------------------------------------------
+
 frame_superior = Frame(chat_ventana, bg="#909497", width=866, height=90, borderwidth=2, relief="solid")
 frame_superior.place(x=265)
+
+photo_logo_clogin = PhotoImage(file="logo.png")
+photo_logo_clogin = photo_logo_clogin.subsample(12)
+logo_clogin = Label(frame_superior, image=photo_logo_clogin, borderwidth=0, bg="#909497")
+logo_clogin.place(x=30)
+# ----------------------------------------------------------------------------------------------------------
+
 
 frame_lateral = Frame(chat_ventana, bg="#057fbc", width=270, height=668, borderwidth=2, relief="solid")
 frame_lateral.place(x=0)
@@ -23,7 +32,6 @@ send_button.place(x=772, y=546)
 
 frame_per_omplir_boto = Frame(frame_conversa, bg="#1A5276", borderwidth=3, width=87)
 frame_per_omplir_boto.place(x=772, y=575)
-
 
 
 
