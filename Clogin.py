b8ea918fab082d483f920c3724077edfc3573a50
equipa6 <equip_a6@boscdelacoma.cat>
2022-05-09 19:43:25 +0200

    si

--- Clogin.py
+++ Clogin.py
@@ -9,27 +9,6 @@ chat_ventana.title("Clogin")
 chat_ventana.geometry("1131x668")
 chat_ventana.resizable(0, 0)
 
-# FRAME DE LES CONFIGURACIONS GENERALS----------------------------------------------------------------------
-"""
-frame_superior = Frame(chat_ventana, bg="#909497", width=866, height=90, borderwidth=2, relief="solid")
-frame_superior.place(x=265)
-
-photo_logo_clogin = PhotoImage(file="logo.png")
-photo_logo_clogin = photo_logo_clogin.subsample(12)
-logo_clogin = Label(frame_superior, image=photo_logo_clogin, borderwidth=0, bg="#909497")
-logo_clogin.place(x=310)
-
-nom_clogin = Label(frame_superior, text="CLOGIN", font=("Calibri", 23, "bold"), bg="#909497")
-nom_clogin.place(x=385, y=20)
-
-ajustes_button = PhotoImage(file="botoajustes.png")
-ajustes_button = ajustes_button.subsample(15)
-label_ajustes_button = Button(frame_superior, image=ajustes_button, borderwidth=0, bg="#909497")
-label_ajustes_button.place(x=800, y=25)
-"""
-
-# ----------------------------------------------------------------------------------------------------------
-
 # Frame Lateral --------------------------------------------------------------------------------------------
 
 frame_lateral = Frame(chat_ventana, bg="#057fbc", width=270, height=668, borderwidth=2, relief="solid")
@@ -40,15 +19,21 @@ photo_logo_clogin = photo_logo_clogin.subsample(20)
 logo_clogin = Label(frame_lateral, image=photo_logo_clogin, borderwidth=0, bg="#057fbc")
 logo_clogin.place(x=60, y=0)
 
-nom_clogin = Label(frame_lateral, text="CLOGIN", font=("Calibri", 15, "bold"), bg="#057fbc")
+nom_clogin = Label(frame_lateral, text="CLOGIN", font=("Calibri", 15, "bold"), bg="#057fbc", fg="#ffffff")
 nom_clogin.place(x=110, y=10)
 
 line_clogin_sota = Frame(frame_lateral, width=266, height=2, bg="black")
 line_clogin_sota.place(x=0, y=50)
 
+imagen_ajustes_generales_button = PhotoImage(file="botoajustes.png")
+imagen_ajustes_generales_button = imagen_ajustes_generales_button.subsample(20)
+ajustes_generales_button = Button(frame_lateral, image=imagen_ajustes_generales_button, borderwidth=0, bg="#057fbc", cursor="hand2", activebackground="#057fbc")
+ajustes_generales_button.place(x=230, y=60)
 
-
-ajustes_foto = PhotoImage()
+foto_afegir_contactes = PhotoImage(file="adduser.png")
+foto_afegir_contactes = foto_afegir_contactes.subsample(20)
+afegir_contactes = Button(frame_lateral, image=foto_afegir_contactes, borderwidth=0, bg="#057fbc", cursor="hand2", activebackground="#057fbc")
+afegir_contactes.place(x=195, y=60)
 
 # ----------------------------------------------------------------------------------------------------------
 
@@ -57,15 +42,15 @@ ajustes_foto = PhotoImage()
 frame_conversa = Frame(chat_ventana, bg="#ffffff", width=863, height=668, borderwidth=2, relief="solid")
 frame_conversa.place(x=268)
 
-frame_usuari = Frame(frame_conversa, bg="#61758B", width=859, height=80, borderwidth=0)
+frame_usuari = Frame(frame_conversa, bg="#4682B4", width=859, height=80, borderwidth=0)
 frame_usuari.place(x=0)
 
-nom_usuari = Label(frame_usuari, bg="#61758B", text="Usuari", font=("Calibri", 20, "bold"))
+nom_usuari = Label(frame_usuari, bg="#4682B4", text="Usuari", font=("Calibri", 20, "bold"))
 nom_usuari.place(x=70, y=18)
 
 foto_usuari_perfil = PhotoImage(file="foto_perfil.png")
 foto_usuari_perfil = foto_usuari_perfil.subsample(10)
-usuari_foto = Label(frame_usuari, image=foto_usuari_perfil, borderwidth=0, bg="#61758B")
+usuari_foto = Label(frame_usuari, image=foto_usuari_perfil, borderwidth=0, bg="#4682B4")
 usuari_foto.place(x=12, y=12)
 
 inp_chat = Entry(frame_conversa, font=("THIN", 19), bg="#2C3E50", fg="#ffffff", width=55, borderwidth=0)
@@ -79,7 +64,7 @@ frame_per_omplir_boto.place(x=772, y=663)
 
 ajustes_button = PhotoImage(file="tres_punts.png")
 ajustes_button = ajustes_button.subsample(19)
-label_ajustes_button = Menubutton(frame_usuari, image=ajustes_button, borderwidth=0, bg="#61758B")
+label_ajustes_button = Menubutton(frame_usuari, image=ajustes_button, borderwidth=0, bg="#4682B4", activebackground="#4682B4", cursor="hand2")
 label_ajustes_button.place(x=810, y=25)
 menu = Menu(label_ajustes_button, tearoff=False, bg="#61758B", fg="#ffffff")
 menu.add_radiobutton(label="Bloquejar", font=("Calibri", 13, "bold"))
